41567f31ded8d9a6138f787ec8a790948317f4b0
martingathu <martin5gathu@gmail.com>
2020-04-12 21:06:59 +0300

    add a highlight on the latest repo

---
+++ src/app/latest-repo.directive.spec.ts
@@ -0,0 +1,8 @@
+import { LatestRepoDirective } from './latest-repo.directive';
+
+describe('LatestRepoDirective', () => {
+  it('should create an instance', () => {
+    const directive = new LatestRepoDirective();
+    expect(directive).toBeTruthy();
+  });
+});
